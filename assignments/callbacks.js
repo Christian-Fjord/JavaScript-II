ec4fc0fcc7a3fc1ea0a06580dd2da1545e2f7677
Christian-Fjord <fjord.christian@gmail.com>
2019-01-20 20:48:54 -0600

    Callbacks.js Finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,46 +21,101 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+// Step 1.1
+function add(x,y) {
+  return x + y;
+}
 
-function getLength(arr, cb) {
+// Step 1.2
+function sub(x,y) {
+  return x -y;
+}
+
+
+// Step 2
+function calculator(x,y,operation) {
+  return operation(x,y);
+
+}
+
+// Step 3
+console.log(calculator(3,4,add));
+
+
+function theLength(length) {
+  console.log(length);
+}
+
+function getLength(arr, callback) {
   // getLength passes the length of the array into the callback.
-  return cb(arr.length);
+  
+  return callback(arr.length);
+
+  
+ 
+}
+
+getLength(items, theLength);
+
+debugger;
+
+function theLast(lastItem) {
+  console.log(lastItem);
+}
+function last(arr, callback) {
+  // last passes the last item of the array into the callback.
+  return callback(arr[arr.length - 1]);
+}
+
+
+last(items, theLast);
+
+debugger;
+
+function theSum(sum) {
+  console.log(sum);
+}
+
+function sumNums(x, y, callback) {
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+ 
+  callback(x+y);
   
 }
 
-getLength(items, function(length) {
-  console.log(length);
-})
+sumNums(3,4,theSum);
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-  let lastArr = items.length - 1;
 
-  return cb(lastArr);
+function theFactor(factor) {
+  console.log(factor);
 }
 
-last(items, function(lastArr) {
-  console.log(lastArr);
-})
-
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-  let theSum = x + y;
-  return cb(theSum);
-}
-
-sumNums(function() {
-  console.log(theSum);
-})
-
-function multiplyNums(x, y, cb) {
+function multiplyNums(x, y, callback) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+
+  return callback(x*y);
+
 }
 
-function contains(item, list, cb) {
+multiplyNums(5,6, theFactor);
+
+
+function isItPresent(present) {
+  console.log(present);
+}
+function contains(item, list, callback) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+    let isPresent = list.includes(item);
+
+    return callback(isPresent);
+  
 }
+
+contains("Gum", items, isItPresent );
+contains("Cars", items, isItPresent);
+
 
 /* STRETCH PROBLEM */
 
